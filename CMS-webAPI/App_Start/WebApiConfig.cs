337817357470c65ebc56e185c0ddaf17431add15
Sunil Kumar <sunil.divyam@gmail.com>
2016-08-04 19:12:08 +0530

    CMS-webAPI | Authentication | OWIN Setup Code Part | Classes | Package Updates

--- CMS-webAPI/App_Start/WebApiConfig.cs
+++ CMS-webAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
 
 namespace CMS_webAPI
 {
@@ -9,6 +10,11 @@ namespace CMS_webAPI
     {
         public static void Register(HttpConfiguration config)
         {
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            config.MapHttpAttributeRoutes();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
