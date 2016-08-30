5c9d0c733c4345f5d8b6fe639648f74f40084423
Sunil Kumar <sunil.divyam@gmail.com>
2016-08-30 18:59:48 +0530

    CMS-webAPI | Enable CORS

--- CMS-webAPI/App_Start/WebApiConfig.cs
+++ CMS-webAPI/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace CMS_webAPI
 {
@@ -14,9 +15,13 @@ namespace CMS_webAPI
         {
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
+           
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
+            
+            //var cors = new EnableCorsAttribute("http://localhost:81", "*", "*");
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             // Web API routes
             config.MapHttpAttributeRoutes();
 
