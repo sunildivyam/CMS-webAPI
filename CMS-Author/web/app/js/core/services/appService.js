006554dcb00e33cd30868d547352f3cfac4c1bd7
Sunil Kumar <sunil.divyam@gmail.com>
2016-11-29 19:51:01 +0530

    CMS_Author | AppService implementation This implements $http GET, POST, PUT, DELETE methods

--- CMS-Author/web/app/js/core/services/appService.js
+++ CMS-Author/web/app/js/core/services/appService.js
@@ -1,42 +1,133 @@
 'use strict';
-/*
-*	appService
-*	Description
-*	appService fetches the data for all $http requests and keeps a cache for each.
-*	And supplies data from cache for subsequent requests unless forced call is requested
+/**
+* @ngdoc service
+* @name raiweb.core.service:appService
+* @description
+*	appService provides functionality of
+* 	$http service by exposing GET, POST, PUT, DELETE etc type of methods
+*	All other serivices in the project should call these methods for all http requests.
+*	The url should be a relative url to APIs that is passed to this service methods.
+* @require $http
 */
 
 (function() {
-	var appService = function($q, $http) {
+	var appService = function($http) {
+		var baseApiUrl = 'https://localhost:44302/api';
 		var cachedReqs = {};
 
-		function getDataFromCache(url) {
-			if (url && typeof url === 'string') {
-				return cachedReqs[url];
-			}
-			return;
-		}
-		function executeDataRequest(url, forced) {
-			var cachedReq = getDataFromCache(url);
-			var defferedObj = $q.defer();
-			if (forced === true || !cachedReq) {
-				$http.get(url).then(function(response) {
-					if (response && response.data && response.status === 200) {
-						cachedReqs[url] = response.data;
-						defferedObj.resolve(cachedReqs[url]);
-					} else {
-						defferedObj.reject(response);
-					}
-				}, function(rejection) {
-					defferedObj.reject(rejection);
-				});
-			} else {
-				defferedObj.resolve(cachedReq);
-			}
 
-			return defferedObj.promise;
+		function getFullUrl(url) {
+			return [baseApiUrl, url].join('/');
 		}
 
+		function executeRequest(config) {
+			return $http(config);
+		}
+
+		function nonGetRequest(requestType, url, data, headers) {
+			var reqHeaders = headers || {};
+			if (!reqHeaders['Content-Type']) {
+				headers['Content-Type'] = 'application/json';
+			}
+
+			var config = {
+				method: requestType,
+				url: getFullUrl(url),
+				data: data,
+				headers: reqHeaders
+			};
+			return executeRequest(config);
+		}
+
+		/**
+		* @ngdoc method
+		* @name get
+		* @methodOf raiweb.core.service:appService
+		* @description
+		* Executes a GET request
+		*
+		* @param {string} url request relative url
+		* @param {object=} params request's GET params object
+		* @param {object=} headers request headers object
+		*
+		* @returns {object} returns a promise object.
+		*/
+		function get(url, params, headers) {
+			var config = {
+				method: 'GET',
+				url: getFullUrl(url),
+				cache: true,
+				params: params,
+				headers: headers
+			};
+			return executeRequest(config);
+		}
+
+		/**
+		* @ngdoc method
+		* @name post
+		* @methodOf raiweb.core.service:appService
+		* @description
+		* Executes a POST request
+		*
+		* @param {string} url request relative url
+		* @param {object=} data request data object
+		* @param {object=} headers request headers object
+		*
+		* @returns {object} returns a promise object.
+		*/
+		function post(url, data, headers) {
+			return nonGetRequest('POST', url, data, headers);
+		}
+
+		/**
+		* @ngdoc method
+		* @name put
+		* @methodOf raiweb.core.service:appService
+		* @description
+		* Executes a PUT request
+		*
+		* @param {string} url request relative url
+		* @param {object=} data request data object
+		* @param {object=} headers request headers object
+		*
+		* @returns {object} returns a promise object.
+		*/
+		function put(url, data, headers) {
+			return nonGetRequest('PUT', url, data, headers);
+		}
+
+		/**
+		* @ngdoc method
+		* @name del
+		* @methodOf raiweb.core.service:appService
+		* @description
+		* Executes a DELETE request
+		*
+		* @param {string} url request relative url
+		* @param {object=} data request data object
+		* @param {object=} headers request headers object
+		*
+		* @returns {object} returns a promise object.
+		*/
+		function del(url, data, headers) {
+			return nonGetRequest('DELETE', url, data, headers);
+		}
+
+		/**
+		* @ngdoc method
+		* @name getErrorMessage
+		* @methodOf raiweb.core.service:appService
+		* @description
+		*	This processes the modelState object returned from the webAPI in response
+		*	to list all error messages.
+		* 	Optionally an html element name can be passed to generate HTML markup for list.
+		*	Then the list can be displayed in HTML.
+		* @param {object} modelState modelState object from response of web API
+		* @param {string=} listHtmlElement html element for list. default is 'li'
+		*
+		* @returns {string} returns list of error messages in HTML format
+		*/
 		function getErrorMessage(modelState, listHtmlElement) {
 			var errorMessage = [];
 			var listHtmlElem = listHtmlElement;
@@ -55,11 +146,14 @@
 		}
 
 		return {
-			requestData: executeDataRequest,
+			get: get,
+			post: post,
+			put: put,
+			del: del,
 			getErrorMessage: getErrorMessage
 		};
 	};
 
-	appService.$inject = ['$q', '$http'];
+	appService.$inject = ['$http'];
 	module.exports = appService;
 })();
