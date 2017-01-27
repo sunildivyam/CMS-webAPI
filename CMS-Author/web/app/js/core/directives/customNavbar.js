12f93c07d7c20af2d052282fa6be79a27b2518e1
Sunil Kumar <sunil.divyam@gmail.com>
2017-01-27 16:47:32 +0530

    CMS_Author | custom Navbar Update

--- CMS-Author/web/app/js/core/directives/customNavbar.js
+++ CMS-Author/web/app/js/core/directives/customNavbar.js
@@ -8,19 +8,61 @@
 	var customNavbar = function() {
 		return {
 			restrict: 'E',
-			replace: true,
+			replace: false,
 			scope: {
 				name: '@',			// used for navid, statename
 				items: '=',			// used for nv items ie. categories, tags etc. entities
+				selectedItemName: '=',
 				title: '@',			// displays brand section title
 				stateName: '@',		// StateName like pub.category etc. to used in the item links
 				className: '@',		// navbar class name eg. navbar-default, bavbar-inverse etc.
 				itemIconClassNamePre: '@',	// icon class for prefixing item, eg. fa fa-list or glyphicon gyphicon-list etc.
 				itemIconClassNamePost: '@',	// icon class for postfixing item, eg. fa fa-list or glyphicon gyphicon-list etc.
-				headerIconClassName: '@'	// icon class for postfixing brand title, eg. fa fa-arrow-right or glyphicon gyphicon-arrow-right etc.
+				preHeaderIconClassName: '@',	// icon class for prefixing brand title, eg. fa fa-pencil or glyphicon gyphicon-pencil etc.
+				postHeaderIconClassName: '@',	// icon class for postfixing brand title, eg. fa fa-triangle-bottom or glyphicon gyphicon-triangle-bottom etc.
+				onSelect: '='
 			},
 			templateUrl: 'core/custom-navbar.html',
-			link: function() {
+			link: function($scope) {
+				$scope.onItemClick = function(event, item) {
+					$scope.selectedItemName = item && item.name;
+
+					if (typeof $scope.onSelect === 'function') {
+						event.preventDefault();
+						$scope.onSelect(event, item);
+					}
+				}
+
+				$scope.$watch('selectedItemName', function(newValue) {
+					setTitleStr(newValue);
+				});
+
+				$scope.$watch('items', function(newValue) {
+					setTitleStr($scope.selectedItemName);
+				});
+
+				function setTitleStr(selectedItemName) {
+					if (!$scope.title) {
+						$scope.titleStr = getItemTitleByName(selectedItemName);
+					} else {
+						$scope.titleStr = angular.copy($scope.title);
+					}
+				}
+
+				function getItemTitleByName(itemName) {
+					var defaultTitle = 'Explore';
+					if ($scope.items instanceof Array) {
+						var foundItems = $scope.items.filter(function(item) {
+							if (item.name === itemName) {
+								return item;
+							}
+						});
+						if (foundItems instanceof Array && foundItems.length > 0) {
+							return foundItems[0].title;
+						}
+					}
+					return defaultTitle;
+				}
 			}
 		};
 	};
