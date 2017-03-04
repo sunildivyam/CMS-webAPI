9c228a51e340cd170f8d950161a4162a06831750
Sunil Kumar <sunil.divyam@gmail.com>
2017-03-04 09:46:07 +0530

    CMS-Author | Roboto Font Added to application

--- CMS-Author/web/js-build/tasks/fonts.js
+++ CMS-Author/web/js-build/tasks/fonts.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(gulp, config) {
 	gulp.task('fonts', function() {
-		gulp.src([config.fonts.app + '/**/*', config.fonts.bootstrap + '/**/*', config.fonts.awesome + '/**/*'])
+		gulp.src([config.fonts.app + '/**/*', config.fonts.bootstrap + '/**/*', config.fonts.awesome + '/**/*', config.fonts.roboto + '/Roboto-Regular.*'])
 		.pipe(gulp.dest(config.targetDir + '/' + config.fonts.app));
 	});
 };
