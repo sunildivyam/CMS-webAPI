fa878863416d233b464c5033a8eeec3b846ed4d8
Sunil Kumar <sunil.divyam@gmail.com>
2017-10-31 15:24:33 +0530

    js-build cleanCss Issue fix

--- CMS-merged/CMS_webAPI/ng_project/js-build/tasks/cleanCss.js
+++ CMS-merged/CMS_webAPI/ng_project/js-build/tasks/cleanCss.js
@@ -8,8 +8,8 @@ var rename = require('gulp-rename');
 
 module.exports = function(gulp, config) {
 	gulp.task('cleanCss', function() {
-        return gulp.src([config.targetRoot, config.stylesDir, config.cssFileName + '.css'].join('/'))
-            .pipe(rename(config.cssFileName + '.min.css'))
+        return gulp.src([config.targetRoot, config.stylesDir, config.less.cssFileName + '.css'].join('/'))
+            .pipe(rename(config.less.cssFileName + '.min.css'))
 		.pipe(cleanCss())
             .pipe(gulp.dest(config.targetRoot + '/' + config.stylesDir));
 	});
