423242da9eaad3a0803ed6e7a4af7cc0b3d0abdd
Sunil Kumar <sunil.divyam@gmail.com>
2017-11-12 22:32:54 +0530

    Route Issue

--- CMS-merged/CMS_webAPI_4.5/CMS-webAPI/App_Start/RouteConfig.cs
+++ CMS-merged/CMS_webAPI_4.5/CMS-webAPI/App_Start/RouteConfig.cs
@@ -21,8 +21,8 @@ namespace CMS_webAPI
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{param1}/{param2}/{param3}/{param4}/{param5}/{param6}",
-                defaults: new { controller = "Home", action = "Index", param1 = UrlParameter.Optional, param2 = UrlParameter.Optional,param3 = UrlParameter.Optional,param4 = UrlParameter.Optional,param5 = UrlParameter.Optional,param6 = UrlParameter.Optional }
+                url: "{controller}/{param1}/{param2}/{param3}/{param4}/{param5}",
+                defaults: new { controller = "Home", action = "Index", param1 = UrlParameter.Optional, param2 = UrlParameter.Optional, param3 = UrlParameter.Optional, param4 = UrlParameter.Optional, param5 = UrlParameter.Optional }
             );
         }
     }
